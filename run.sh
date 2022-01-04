07f6188740571fd292155e09af38a3dcc91d2e47
Frederico J. R. Pelogia <fredpelogia@outlook.com>
2022-01-04 16:48:43 -0300

    modifica script de execucao

--- run.sh
+++ run.sh
@@ -2,6 +2,10 @@
 if [[ $1 > 4 ]] 
 then
     mpicc mpi_gol.c -lm -O3 && mpirun --use-hwthread-cpus --oversubscribe -np $1 a.out
-else
+elif [[ $1 > 2 ]]
+then
+    echo "mai2"
     mpicc mpi_gol.c -lm -O3 && mpirun --use-hwthread-cpus -np $1 a.out
+else
+    mpicc mpi_gol.c -lm -O3 && mpirun  -np $1 a.out
 fi
